83e4f72a75cbf03f91fbf4ab8aa58c35b412ee75
Alexandra <asjlobaton@gmail.com>
2025-11-05 19:02:06 +0300

    Create double-check-number.py

---
+++ double-check-number.py
@@ -0,0 +1,12 @@
+num = int(input("Enter a number to check: "))
+
+if num > 50: 
+    print("Number is greater than 50")
+    
+    if num%2==0:
+        print("It is even too")
+    else: 
+        print("It is odd")
+
+else: 
+    print("Number is less than 50)")
