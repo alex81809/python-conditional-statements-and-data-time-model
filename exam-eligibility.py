733099a0095250267ce9fef235862fb4c379c780
Alexandra <asjlobaton@gmail.com>
2025-11-06 09:56:27 +0300

    Create exam-eligibility.py

---
+++ exam-eligibility.py
@@ -0,0 +1,9 @@
+# GPA
+gpa = int(input("What is your GPA?: "))
+
+if gpa >= 70: 
+    print("You will enter the exam")
+
+else: 
+    print("You have failed to meet the requirements to take the exam")
+
